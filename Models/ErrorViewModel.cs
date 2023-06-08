dccdb814b3c84801004b7d8842db7ff50d6deab5
Robson Teles <robson.teles@flex.com>
2023-06-08 16:35:19 -0300

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloWorld_aspNetCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
